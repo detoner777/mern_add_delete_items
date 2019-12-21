28bc05f284af06b05e4c50dcd6c6d83b19c15000
Alexey_Lisenko <detoner777@gmail.com>
2019-12-21 21:38:06 +0200

    db is conneting

--- database/dbConection.js
+++ database/dbConection.js
@@ -1,8 +1,3 @@
 module.exports = {
-  connection:
-    "mongodb://" +
-    process.env.DB_USER +
-    ":" +
-    process.env.DB_PASS +
-    "@mern-crad-demo-xo0xm.mongodb.net/test?retryWrites=true&w=majority"
+  connection: process.env.DATABASE_CLOUD
 };
